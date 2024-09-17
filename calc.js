b24416aece6facac458731c4a836eda15427e425
KumarDishant20 <162104510+KumarDishant20@users.noreply.github.com>
2024-09-18 04:01:35 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,23 @@
+ const display=document.querySelector("#display");
+ 
+ 
+ function appendToDisplay(input){
+    display.value+=input;
+ }
+
+
+ function clearDisplay(){
+display.value="";
+ }
+
+ function calculate(){
+    try{
+        display.value=eval(display.value);
+
+    }
+    catch(err){
+        display.value="Error";
+        }
+        
+    }
+ 
